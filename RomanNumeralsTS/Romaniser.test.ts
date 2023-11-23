c0103127a16aeefa13d42f33d4bd08ee03bbfac3
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-11-23 09:32:23 +0000

    Add jest API features

--- RomanNumeralsTS/Romaniser.test.ts
+++ RomanNumeralsTS/Romaniser.test.ts
@@ -1,50 +1,45 @@
 import { Romaniser } from "./Romaniser";
 
 describe("Romaniser ", () => {
-    it("returns I for 1", () => {
-        const sut = new Romaniser();
 
-        const result = sut.Romanise(1);
+    let _sut;
 
-        expect(result).toBe("I");
+    beforeEach(() => {
+        _sut = new Romaniser();
+    });
+
+    it.each([[1,"I"]])("returns I for 1", (arabic, roman) => {
+        const result = _sut.Romanise(arabic);
+
+        expect(result).toBe(roman);
     });
 
     it("returns II for 2", () => {
-        const sut = new Romaniser();
-
-        const result = sut.Romanise(2);
+        const result = _sut.Romanise(2);
 
         expect(result).toBe("II");
     });
 
     it("returns III for 3", () => {
-        const sut = new Romaniser();
-
-        const result = sut.Romanise(3);
+        const result = _sut.Romanise(3);
 
         expect(result).toBe("III");
     });
 
     it("returns IV for 4", () => {
-        const sut = new Romaniser();
-
-        const result = sut.Romanise(4);
+        const result = _sut.Romanise(4);
 
         expect(result).toBe("IV");
     });
 
     it("returns V for 5", () => {
-        const sut = new Romaniser();
-
-        const result = sut.Romanise(5);
+        const result = _sut.Romanise(5);
 
         expect(result).toBe("V");
     });
 
     it("returns VI for 6", () => {
-        const sut = new Romaniser();
-
-        const result = sut.Romanise(6);
+        const result = _sut.Romanise(6);
 
         expect(result).toBe("VI");
     });
